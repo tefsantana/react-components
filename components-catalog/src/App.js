9ae130325f5174603ec9414d72164905e2585949
tefsantana <esantana@fi.uba.ar>
2022-07-04 04:43:53 -0300

    main: Some technologies, such as Tailwind CSS with PostCSS are introduced in the project.
    - Showing the objectives of this project and a list with the technologies used.
    
    Also, file 'content.js' has been created to have some constants variables to be used.

--- components-catalog/src/App.js
+++ components-catalog/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [items, setItems] = useState([]); // array of items that will be the components of the main section.
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content font-sans text-left">
+        <h1 className="">
+          Web Components Catalog
+        </h1>
+        <h3>About this project.</h3>
+        <div className=''>
+          <span>The main objective of this project is to show you all the components that I have created.</span>
+          <ul>
+            <li>Of course, this project is a public one so everyone can use it as help to guide themselves in the usage of React.</li>
+            <li>Also, I'll try to use coding best practices to reach a good readability.</li>
+          </ul>
+        </div>
+        <div className='text-lg'>
+          <h3>This project is made with:</h3>
+          <ul>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>Javascript React (using Hooks)</li>
+            <li>Tailwind CSS</li>
+            <li>Webpack</li>
+            <li>Babel</li>
+            <li>ESLint</li>
+            <li>Jest</li>
+          </ul>
+        </div>
+      </section>
     </div>
   );
 }
---
+++ components-catalog/src/content.js
@@ -0,0 +1,1 @@
+// The main objective of this file is to declarate the constants that will be used in the App.js file.
